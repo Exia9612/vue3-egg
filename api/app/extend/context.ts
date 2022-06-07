ca4527cbdc8b01103ae0722c89d85fd9f4a79c5d
Qipeng cheng <565644830@qq.com>
2022-06-07 22:01:20 +0800

    setup eggjs

--- api/app/extend/context.ts
+++ api/app/extend/context.ts
@@ -1,7 +1,13 @@
 const nodeFetch = require('node-fetch')
+import { formatParams } from '../lib/utils'
+import { IHttpGetParams } from '../../typings'
 
 module.exports = {
-  httpGet(): Promise<void> {
-
+  httpGet(options: IHttpGetParams): Promise<void> {
+    const { url, data, success, fail } = options
+    return nodeFetch(url, formatParams(data, this.app.config.APP_KEY))
+      .then((res) => res.json())
+      .then((data) => success(data.result.data))
+      .catch((err) => fail(err))
   }
 }
