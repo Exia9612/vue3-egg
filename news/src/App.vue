cbcef0895801bea52b5535c662aa2a0819dd6425
Qipeng cheng <565644830@qq.com>
2022-06-12 23:44:27 +0800

    complete header

---
+++ news/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <my-header />
+    <!-- Component：要渲染的组件 -->
+    <router-view v-slot="{ Component }">
+      <!-- include - string | RegExp | Array。只有名称匹配的组件会被缓存 -->
+      <keep-alive include="Home">
+        <component :is="Component" />
+      </keep-alive>
+    </router-view>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/Header/index.vue';
+export default {
+  name: 'App',
+  components: {
+    MyHeader
+  }
+}
+</script>
